7cceb0c32435f1a7caf57ae49d93d9bad585f4ee
417242 <115142420+417242@users.noreply.github.com>
2022-11-10 16:39:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-let x = 0
-let y = 0
+let x = 2
+let dx = 1
+let _2x = 1
+let _2dx = 1
+let _3x = 0
+let _3dx = 1
 basic.forever(function () {
-    x = 0
-    y = 0
-    led.plot(0, 0)
-    led.plot(0, 1)
-    led.plot(0, 2)
-    led.plot(0, 3)
-    led.plot(0, 4)
-    basic.pause(100)
-    led.plot(x + 1, 0)
-    led.plot(x + 1, 1)
-    led.plot(x + 1, 2)
-    led.plot(x + 1, 3)
-    led.plot(x + 1, 4)
+	
 })
